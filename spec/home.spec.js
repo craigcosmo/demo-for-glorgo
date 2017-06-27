3db5d4a02b21a010ae9a794ddd463b7af863ca37
Craig Nguyen <craigcosmo@gmail.com>
2017-06-27 15:26:10 +0700

    d

--- spec/home.spec.js
+++ spec/home.spec.js
@@ -2,10 +2,16 @@ import Home from 'Home'
 import React from 'react'
 import {expect} from 'chai'
 import { shallow } from 'enzyme'
+import ReactShallowRenderer from 'react-test-renderer/shallow'
+
+
 
 describe('home', () => {
 	it('should be working', () => {
-		const wrapper = shallow(<Home />)
-		expect(wrapper.length).to.equal(1)
+		const renderer = new ReactShallowRenderer()
+		renderer.render(<Home />)
+		const result = renderer.getRenderOutput()
+		console.log('aaa',result.type)
+		expect(result.type).to.equal('div')
 	})
 })
